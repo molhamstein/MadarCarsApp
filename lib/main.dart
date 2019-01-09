247ff684d93fecc7c20e7b295022f90af795c5b8
DaRkViRuS2012 <nour_araar_2009@yahoo.com>
2019-01-09 15:34:48 +0200

    add settings page

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,16 @@ import 'package:madar_booking/bloc_provider.dart';
 import 'package:madar_booking/auth_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'DataStore.dart';
+import 'settings_page.dart';
 
 void main() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
-  //DataStore.init();
   runApp(MyApp(prefs: prefs));
 }
 
 class MyApp extends StatelessWidget {
   final SharedPreferences prefs;
-  int counter = 0; //DataStore().counter ?? 0;
+  int counter = 0;
 
   MyApp({Key key, this.prefs})
       : assert(prefs != null),
@@ -22,24 +22,17 @@ class MyApp extends StatelessWidget {
 
   void incress() {
     print(counter);
-    //  DataStore().counter++;
+    counter++;
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
-          body: Center(
-            child: Container(
-              height: 100,
-              width: 100,
-              color: Colors.red,
-            ),
-          ),
-        ));
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SettingsPage(),
+    );
   }
 }
---
+++ lib/settings_page.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import 'package:madar_booking/madar_colors.dart';
+
+class SettingsPage extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MySettingsPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MySettingsPage extends StatefulWidget {
+  MySettingsPage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MySettingsPageState createState() => _MySettingsPageState();
+}
+
+class _MySettingsPageState extends State<MySettingsPage> {
+  void _incrementCounter() async {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: Container(
+            decoration:
+                BoxDecoration(gradient: MadarColors.gradiant_decoration),
+          ),
+        ),
+        Expanded(
+            flex: 2,
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 40.0, bottom: 40.0),
+                      child: Text(
+                        "Some Slogan about Madar",
+                        style: TextStyle(
+                            fontSize: 17, color: Colors.grey.shade700),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 32.0, right: 32.0),
+                    child: Container(
+                      height: 180,
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(
+                                        color:
+                                            Theme.of(context).dividerColor))),
+                            height: 60,
+                            child: InkWell(
+                              onTap: () {
+                                print("pressed");
+                              },
+                              child: Row(
+                                children: <Widget>[
+                                  Text(
+                                    "Contact us",
+                                    style: TextStyle(fontSize: 17),
+                                  ),
+                                  Expanded(
+                                    child: Container(),
+                                  ),
+                                  Icon(Icons.chevron_right)
+                                ],
+                              ),
+                            ),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(
+                                        color:
+                                            Theme.of(context).dividerColor))),
+                            height: 60,
+                            child: InkWell(
+                              onTap: () {
+                                print("pressed");
+                              },
+                              child: Row(
+                                children: <Widget>[
+                                  Text(
+                                    "Privacy Policy",
+                                    style: TextStyle(fontSize: 17),
+                                  ),
+                                  Expanded(
+                                    child: Container(),
+                                  ),
+                                  Icon(Icons.chevron_right)
+                                ],
+                              ),
+                            ),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(
+                                        color:
+                                            Theme.of(context).dividerColor))),
+                            height: 60,
+                            child: InkWell(
+                              onTap: () {
+                                print("pressed");
+                              },
+                              child: Row(
+                                children: <Widget>[
+                                  Text(
+                                    "Push Notifications",
+                                    style: TextStyle(fontSize: 17),
+                                  ),
+                                  Expanded(
+                                    child: Container(),
+                                  ),
+                                  Icon(Icons.chevron_right)
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(32.0),
+                          child: Container(
+                            constraints: BoxConstraints.expand(height: 50),
+                            child: FlatButton(
+                              onPressed: () {
+                                print("logout");
+                              },
+                              child: Text(
+                                'Logout',
+                                style: TextStyle(
+                                    fontSize: 24.0, color: Colors.black),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )),
+      ],
+    )
+        // This trailing comma makes auto-formatting nicer for build methods.
+        );
+  }
+}
