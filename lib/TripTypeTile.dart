fd07f6c9262cb848ec09a7fd1726207feacaa510
Ayham Orfali <orfali.ayham@gmail.com>
2019-01-24 15:54:59 +0200

    localized step two

--- lib/TripTypeTile.dart
+++ lib/TripTypeTile.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:madar_booking/madarLocalizer.dart';
 
 class TripTypeTile extends StatefulWidget {
-
   final IconData iconData;
   final String title;
   final Function(bool) onChecked;
   final bool checked;
 
-  const TripTypeTile({Key key, this.iconData, this.title, this.onChecked, this.checked = false}) : super(key: key);
+  const TripTypeTile(
+      {Key key,
+      this.iconData,
+      this.title,
+      this.onChecked,
+      this.checked = false})
+      : super(key: key);
 
   @override
   TripTypeTileState createState() {
@@ -17,7 +23,6 @@ class TripTypeTile extends StatefulWidget {
 }
 
 class TripTypeTileState extends State<TripTypeTile> {
-
   bool _checked;
 
   @override
@@ -33,21 +38,20 @@ class TripTypeTileState extends State<TripTypeTile> {
       height: 80,
       margin: EdgeInsets.all(16),
       decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black26,
-            blurRadius: 15,
-          ),
-        ],
-        borderRadius: BorderRadius.circular(15)
-      ),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 15,
+            ),
+          ],
+          borderRadius: BorderRadius.circular(15)),
       child: Material(
         color: Colors.transparent,
         child: InkWell(
           borderRadius: BorderRadius.circular(15),
           splashColor: Colors.yellow[700],
-          onTap: (){
+          onTap: () {
             setState(() {
               _checked = !_checked;
               widget.onChecked(_checked);
@@ -57,7 +61,15 @@ class TripTypeTileState extends State<TripTypeTile> {
             padding: EdgeInsets.all(16),
             child: Stack(
               children: <Widget>[
-                _checked ? Align(alignment: Alignment.topRight, child: Icon(FontAwesomeIcons.solidCheckCircle, color: Colors.yellow[700],)) : Container(),
+                _checked
+                    ? Align(
+                        alignment: MadarLocalizations.of(context).locale.languageCode == 'en' ? Alignment.topRight : Alignment.topLeft,
+                        child: Icon(
+                          FontAwesomeIcons.solidCheckCircle,
+                          color: Colors.yellow[700],
+                        ),
+                      )
+                    : Container(),
                 Center(
                   child: Row(
                     mainAxisSize: MainAxisSize.max,
@@ -66,8 +78,13 @@ class TripTypeTileState extends State<TripTypeTile> {
                     children: <Widget>[
                       Icon(widget.iconData),
                       Padding(
-                        padding: const EdgeInsets.only(left: 32.0),
-                        child: Text(widget.title, style: TextStyle(color: Colors.black87, fontWeight: FontWeight.w700),),
+                        padding: const EdgeInsets.only(left: 32.0, right: 32),
+                        child: Text(
+                          widget.title,
+                          style: TextStyle(
+                              color: Colors.black87,
+                              fontWeight: FontWeight.w700),
+                        ),
                       ),
                     ],
                   ),
