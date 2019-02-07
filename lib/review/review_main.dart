b6bdaeedd35a61462dc2ded0ef0baa554736bb3a
Ayham Orfali <orfali.ayham@gmail.com>
2019-02-07 14:48:28 +0200

    finished review ui

--- lib/review/review_main.dart
+++ lib/review/review_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:madar_booking/madarLocalizer.dart';
 import 'package:madar_booking/review/expressions.dart';
 import 'package:madar_booking/review/review_bloc.dart';
 
@@ -9,10 +10,16 @@ class ReviewMain extends StatefulWidget {
   }
 }
 
-class ReviewMainState extends State<ReviewMain> {
+class ReviewMainState extends State<ReviewMain> with TickerProviderStateMixin {
   PageController pageController;
   Animatable<Color> background;
   ReviewBloc bloc;
+  Animation<double> badFace;
+  Animation<double> okFace;
+  Animation<double> greatFace;
+  AnimationController badController;
+  AnimationController okController;
+  AnimationController greatController;
 
   @override
   void initState() {
@@ -22,11 +29,28 @@ class ReviewMainState extends State<ReviewMain> {
   }
 
   void _initialize() {
+    badController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 100),
+    );
+    okController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 100),
+    );
+    greatController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 100),
+    );
+
+    badFace = Tween(begin: 0.0, end: 1.0).animate(badController);
+    okFace = Tween(begin: 0.0, end: 1.0).animate(okController);
+    greatFace = Tween(begin: 0.0, end: 1.0).animate(greatController);
+
     background = TweenSequence<Color>([
       TweenSequenceItem(
         weight: 1.0,
         tween: ColorTween(
-          begin: Colors.red,
+          begin: Colors.red[400],
           end: Colors.orange,
         ),
       ),
@@ -34,27 +58,48 @@ class ReviewMainState extends State<ReviewMain> {
         weight: 1.0,
         tween: ColorTween(
           begin: Colors.orange,
-          end: Colors.yellow,
+          end: Colors.yellow[700],
         ),
       ),
       TweenSequenceItem(
         weight: 1.0,
         tween: ColorTween(
-          begin: Colors.yellow,
-          end: Colors.greenAccent,
+          begin: Colors.yellow[700],
+          end: Colors.teal[300],
         ),
       ),
       TweenSequenceItem(
         weight: 1.0,
         tween: ColorTween(
-          begin: Colors.greenAccent,
-          end: Colors.green,
+          begin: Colors.teal[300],
+          end: Colors.teal[400],
         ),
       ),
     ]);
     pageController = PageController();
-    pageController.addListener((){
+    if (pageController.page == 0) {
+      badController.forward();
+      okController.reverse();
+      greatController.reverse();
+    }
+    pageController.addListener(() {
       bloc.pushIndex(pageController.page);
+
+      if (pageController.page <= 1) {
+        badController.forward();
+        okController.reverse();
+        greatController.reverse();
+      }
+      if (pageController.page < 3 && pageController.page >= 2) {
+        badController.reverse();
+        okController.forward();
+        greatController.reverse();
+      }
+      if (pageController.page >= 3) {
+        badController.reverse();
+        okController.reverse();
+        greatController.forward();
+      }
     });
   }
 
@@ -67,7 +112,6 @@ class ReviewMainState extends State<ReviewMain> {
 
   @override
   Widget build(BuildContext context) {
-
     final duration = Duration(milliseconds: 200);
 
     return Scaffold(
@@ -78,24 +122,24 @@ class ReviewMainState extends State<ReviewMain> {
               return Stack(
                 alignment: Alignment.bottomCenter,
                 children: <Widget>[
+
                   AnimatedBuilder(
                     animation: pageController,
                     builder: (context, child) {
-
-                      if(pageController.hasClients) {
+                      if (pageController.hasClients) {
                         print(pageController.page);
                       }
 
                       return DecoratedBox(
                         decoration: BoxDecoration(
-                          color: background
-                              .evaluate(AlwaysStoppedAnimation(snapshot.data / 4)),
+                          color: background.evaluate(
+                              AlwaysStoppedAnimation(snapshot.data / 5)),
                         ),
                         child: child,
                       );
                     },
                     child: PageView(
-//                      physics: NeverScrollableScrollPhysics(),
+                      physics: NeverScrollableScrollPhysics(),
                       controller: pageController,
                       children: [
                         Center(child: Text("")),
@@ -106,9 +150,34 @@ class ReviewMainState extends State<ReviewMain> {
                       ],
                     ),
                   ),
-                  CustomPaint(
-                    size: MediaQuery.of(context).size,
-                    painter: SmilePainter(pageController.hasClients ? pageController.page * 100 : 0),
+                  Container(
+                      margin: EdgeInsets.all(32),
+                      alignment: Alignment.center,
+                      child: Stack(
+                        children: <Widget>[
+                          FadeTransition(
+                              opacity: badFace,
+                              child: Image.asset('assets/images/bad_face.png')),
+                          FadeTransition(
+                              opacity: okFace,
+                              child: Image.asset('assets/images/ok_face.png')),
+                          FadeTransition(
+                              opacity: greatFace,
+                              child:
+                                  Image.asset('assets/images/great_face.png')),
+                        ],
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.only(top:80.0),
+                    child: Align(
+                      alignment: Alignment.topCenter,
+                      child: Text(
+                        MadarLocalizations.of(context).trans('review_text'),
+                        style: TextStyle(
+                          fontSize: 22,
+                            color: Colors.white, fontWeight: FontWeight.w700),
+                      ),
+                    ),
                   ),
                   Padding(
                     padding: const EdgeInsets.only(bottom: 60.0),
@@ -119,7 +188,8 @@ class ReviewMainState extends State<ReviewMain> {
                         children: <Widget>[
                           InkWell(
                             borderRadius: BorderRadius.circular(30),
-                            onTap: () => pageController.animateToPage(0, duration: duration, curve: Curves.linear),
+                            onTap: () => pageController.animateToPage(0,
+                                duration: duration, curve: Curves.linear),
                             splashColor: Colors.white,
                             child: Padding(
                               padding: const EdgeInsets.all(16.0),
@@ -135,7 +205,8 @@ class ReviewMainState extends State<ReviewMain> {
                           ),
                           InkWell(
                             borderRadius: BorderRadius.circular(30),
-                            onTap: () => pageController.animateToPage(1, duration: duration, curve: Curves.linear),
+                            onTap: () => pageController.animateToPage(1,
+                                duration: duration, curve: Curves.linear),
                             splashColor: Colors.white,
                             child: Padding(
                               padding: const EdgeInsets.all(16.0),
@@ -151,7 +222,8 @@ class ReviewMainState extends State<ReviewMain> {
                           ),
                           InkWell(
                             borderRadius: BorderRadius.circular(30),
-                            onTap: () => pageController.animateToPage(2, duration: duration, curve: Curves.linear),
+                            onTap: () => pageController.animateToPage(2,
+                                duration: duration, curve: Curves.linear),
                             splashColor: Colors.white,
                             child: Padding(
                               padding: const EdgeInsets.all(16.0),
@@ -167,7 +239,8 @@ class ReviewMainState extends State<ReviewMain> {
                           ),
                           InkWell(
                             borderRadius: BorderRadius.circular(30),
-                            onTap: () => pageController.animateToPage(3, duration: duration, curve: Curves.linear),
+                            onTap: () => pageController.animateToPage(3,
+                                duration: duration, curve: Curves.linear),
                             splashColor: Colors.white,
                             child: Padding(
                               padding: const EdgeInsets.all(16.0),
@@ -183,9 +256,8 @@ class ReviewMainState extends State<ReviewMain> {
                           ),
                           InkWell(
                             borderRadius: BorderRadius.circular(30),
-                            onTap: () {
-                              print('asdasdads');
-                            },
+                            onTap: () => pageController.animateToPage(4,
+                                duration: duration, curve: Curves.linear),
                             splashColor: Colors.white,
                             child: Padding(
                               padding: const EdgeInsets.all(16.0),
@@ -203,7 +275,6 @@ class ReviewMainState extends State<ReviewMain> {
                       ),
                     ),
                   ),
-
                 ],
               );
             }));
