1849fe005d3053b9b4f12f7575b30cf6153eccd7
Ayham Orfali <orfali.ayham@gmail.com>
2019-02-26 16:21:47 +0200

    fix

--- lib/madar_fonts.dart
+++ lib/madar_fonts.dart
@@ -1,8 +1,8 @@
 class AppFonts {
-  static const double x_small_font_size = 8;
-  static const double small_font_size = 14;
-  static const double medium_font_size = 16;
-  static const double normal_font_size = 20;
-  static const double large_font_size = 24;
-  static const double x_large_font_size = 50;
+  static const double x_small_font_size = 8.0;
+  static const double small_font_size = 14.0;
+  static const double medium_font_size = 16.0;
+  static const double normal_font_size = 20.0;
+  static const double large_font_size = 24.0;
+  static const double x_large_font_size = 50.0;
 }
