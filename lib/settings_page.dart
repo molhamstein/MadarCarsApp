c0a87b6cde1a1eac3f25b71178f6b4f629ce583e
Ayham Orfali <orfali.ayham@gmail.com>
2019-03-03 15:27:45 +0200

    Contact us remove the predefined message "helloo there"

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -14,7 +14,7 @@ class MySettingsPage extends StatefulWidget {
 }
 
 class _MySettingsPageState extends State<MySettingsPage> {
-  String _contactusURL = "mailto:info@jawlatcom.com?subject=helloo there&body=";
+  String _contactusURL = "mailto:info@jawlatcom.com?body=";
   String _privacyURL = "https://jawlatcom.com/privacy";
   // const _contactusURL = "";
 
