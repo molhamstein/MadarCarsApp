cc530f5bf7eaeece7f260452a05e71ca3158240c
Molham Mahmoud <fatherboard1@gmail.com>
2020-02-19 17:35:03 +0200

    prepare for iOS submit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/molhamStein/Documents/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/molhamStein/Documents/Flutter/MadarCarsApp"
+export "FLUTTER_TARGET=/Users/molhamStein/Documents/Flutter/MadarCarsApp/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/molhamStein/Documents/Flutter/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.8.0"
+export "FLUTTER_BUILD_NUMBER=15"
+export "TRACK_WIDGET_CREATION=true"
